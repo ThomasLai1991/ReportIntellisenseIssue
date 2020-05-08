8761297d70d13021ca6cc358873ffc05c22782ec
Thomas Lai <thomasadam1991@gmail.com>
2020-05-08 14:44:05 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const app = express();
+
+app.get();
+
+setInter
+
+
+
+
+
+
+
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.static("public"));
+
+// app.set("view engine", "ejs");
+
+
+// app.get("/", function name(req, res) {
+
+//   res
+//   var today = new Date();
+
+
+//   var option={
+//       weekday:'long',
+//       day:'numeric',
+//       month:'long'
+//   }
+
+  
+
+//   var day= today.toLocaleDateString()
+// //   var currentDay = "";
+
+// //   switch (today.getDay()) {
+// //     case 0:
+// //       currentDay = "Sunday";
+// //       break;
+// //     case 1:
+// //       currentDay = "Monday";
+// //       break;
+// //     case 2:
+// //       currentDay = "Tuesday";
+// //       break;
+// //     case 3:
+// //       currentDay = "Wednesday";
+// //       break;
+// //     case 4:
+// //       currentDay = "Thursday";
+// //       break;
+// //     case 5:
+// //       currentDay = "Friday";
+// //       break;
+// //     case 6:
+// //       currentDay = "Saturday";
+// //       break;
+
+// //     default:
+// //       console.log("Error");
+// //       break;
+// //   }
+
+//   res.render("list", { currentDay: currentDay });
+// });
+
+// app.listen(3000, function() {
+//   console.log("Server listening on port 3000");
+// });
